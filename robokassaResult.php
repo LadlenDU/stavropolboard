5f4f09a6e615a1a4b5b5a76127d35bbe2184fbb3
Dolgov Dmitriy <TwilightTower@mail.ru>
2017-07-15 07:37:07 +0300

    sql for robokassa

--- robokassaResult.php
+++ robokassaResult.php
@@ -9,21 +9,29 @@ $mrh_pass2 = 'putYourPassword#2Here';
 $crc = md5("$_GET[OutSum]:$_GET[InvId]:$mrh_pass2:shp_Item=$_GET[shp_Item]");
 
 if ($_GET['SignatureValue'] == $crc) {
+    $sql = '';
+
     $subject = 'Получена оплата';
     $msg = "От пользователя получена оплата\n\n"
         . "Цена: $_GET[OutSum]\n"
         . "ID объявления: $_GET[InvId]\n";
     if ($_GET['shp_Item'] == 'vip') {
         $msg .= 'Тип: присвоение VIP статуса';
+        $sql = "update jb_board set checkbox_select=1, select_time=NOW() where id='" . mysql_real_escape_string($_GET['InvId']) . "'";
     } elseif ($_GET['shp_Item'] == 'sel') {
         $msg .= 'Тип: выделение объявления';
+        $sql = "update jb_board set checkbox_top=1, top_time=NOW() where id='" . mysql_real_escape_string($_GET['InvId']) . "'";
     }
+
+    mysql_query($sql);
+
     sendmailer(ROBOKASSA_RESULT_EMAIL, $c['admin_mail'], $subject, $msg);
+
     echo 'OK' . $_GET['InvId'];
 } else {
     $subject = 'ROBOKASSA - неверная КС';
     $msg = "ROBOKASSA вернул результат с неправильной контрольной суммой.\nПараметры $_GET:\n"
         . print_r($_GET, true);
     sendmailer(ROBOKASSA_RESULT_EMAIL, $c['admin_mail'], $subject, $msg);
-    echo 'wrond checksum: ' . $_GET['InvId'];
+    echo 'wrong checksum: ' . $_GET['InvId'];
 }
